cafe7d879547f9cbc20c71d48e13243ecb65657d
Javed-Shahbaz <javedshahbaz007@gmail.com>
2024-02-19 22:24:17 +0500

    setup components

---
+++ src/Navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Welcome</h1>
+      <nav>
+        <a href="Home">Home</a>
+        <a href="contact">Contact</a>
+        <a href="apps">Apps</a>
+      </nav>
+    </div>
+  );
+};
